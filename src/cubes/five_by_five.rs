a40c681c8476f1a88e957bb6137df930495c18aa
John Brosnan <brosnan@adobe.com>
2021-09-28 16:07:35 -0400

    Add more 5x5 patterns

--- src/cubes/five_by_five.rs
+++ src/cubes/five_by_five.rs
@@ -1,7 +1,9 @@
 pub fn display_pattern(name: &str) {
     let pattern : &str = match name {
-        "list" => "cube-in-cube",
+        "list" => "cube-in-cube\nhearts\npeak",
         "cube-in-cube" =>  "F U' B L U' F2 U2 F U F' U2 D' B D L2 B2 U Fw Uw' Bw Lw Uw' Fw2 Uw2 Fw Uw Fw' Uw2 Dw' Bw Dw Lw2 Bw2 Uw",
+        "hearts" => "M2 2-4Rw2 S2 D 2-4Fw2 D2 2-4Fw2 D S2 2-4Rw M2 2D 2-4Rw 2-4Dw2 2-4Rw' 2D 2-4Rw",
+        "peak" => "F' 2L2 F R2 2B U' 2B' Dw' 2B U 2B' Dw R2 F' 2L2 F2 R U' M2 U R' U' M2 U F'",
         _x => "unknown pattern",
     };
     println!("{}", pattern);
